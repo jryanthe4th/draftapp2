54aceefaf2e58ca461762b6a79ab1124746685dd
Joseph Ryan <joseph.ryan.iv@gmail.com>
2018-04-22 13:59:35 -0700

    adding untracked files now that repo is private

---
+++ config.js
@@ -0,0 +1,8 @@
+var fs = require('fs');
+
+var configPath = './config.json';
+var parsed = JSON.parse(fs.readFileSync(configPath, 'UTF-8'));
+
+// We have to export each object in order to access them separately
+exports.mailgun = parsed.mailgun;
+exports.port = parsed.port;

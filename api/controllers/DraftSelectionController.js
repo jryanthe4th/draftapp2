7f2ea57512607dfdb74251c2ff1da6dbcaa218e3
Joseph Ryan <joseph.ryan.iv@gmail.com>
2018-04-29 17:40:41 -0700

    added sms integration to fire text on draft selection

--- api/controllers/DraftSelectionController.js
+++ api/controllers/DraftSelectionController.js
@@ -8,27 +8,57 @@
 module.exports = {
 
     draftboard: function(req, res) {
+        // Require config variables for email and sms
         var appConfig = require('../../config.js');
-        var mailgun = require('mailgun-js')({
-            apiKey: appConfig.mailgun.apiKey,
-            domain: appConfig.mailgun.domain
-        });
-        var members = [
-            'hardmoney@mg.dynastydraftboard.com'
-        ];
+        // Email and SMS content
         var draftData = [
             'A draft selection has been submitted',
             'Player: '+req.param('playerName'),
             'Position: '+req.param('position'),
             'Drafted By: '+req.session.firstName
         ];
-        var data = {
+        // Mailgun email sent on draftpick selection
+        var mailgun = require('mailgun-js')({
+            apiKey: appConfig.mailgun.apiKey,
+            domain: appConfig.mailgun.domain
+        });
+        // Mailgun user mailing list group managed on mailgun site
+        var members = [
+            'hardmoney@mg.dynastydraftboard.com'
+        ];
+        // Email config
+        var emailData = {
             from: 'DynastyDraftBoard <mailgun@dynastydraftboard.com>',
             to: members,
             subject: 'Hard Money 2018 Rookie Draft Selection',
             html: draftData
         };
+        // Twilio SMS sent on draftpick selection
+        var accountSid = 'AC9b0bb20c76a79bf9ca663990b6adccb9';
+        var authToken = 'e0b946ca78a2901e2dfa392f8b079bc3';
+        var twilio = require('twilio')(accountSid, authToken);
+        var numbers = [
+            15596180337,
+            15593922960,
+            15592898848,
+            15593497192,
+            15592882642,
+            15593922961,
+            15592882982,
+            15592885147,
+            15592697401,
+            15599085243,
+            15597088081,
+            15593347101,
+        ]
+        // SMS config
+        // var smsData = {
+        //     from: '+17755021662',
+        //     to: number,
+        //     body: draftData
+        // };
 
+        // Create record in draftselection db table
         DraftSelection.create({
 
             playerName: req.param('playerName'),
@@ -41,9 +71,28 @@ module.exports = {
                 console.log("err: ", err);
             }
 
-            mailgun.messages().send(data, function(error, body) {
-                console.log(body);
+            // send mailgun email
+            mailgun.messages().send(emailData, function(error, html) {
+                console.log(html);
             });
+
+            // send twilio sms
+            Promise.all(
+                numbers.map(number => {
+                    return twilio.messages.create({
+                        from: '+17755021662',
+                        to: number,
+                        body: draftData
+                    });
+                })
+            )
+            .then(messages => {
+                console.log('SMS messages sent!');
+            })
+            .catch(err => console.error(err));
+
+            // twilioClient.messages.create(smsData)
+            // .then(message => console.log(message.sid));
 
             return res.json({
                 id: newDraftSelection.id
--- config.js
+++ config.js
@@ -6,3 +6,4 @@ var parsed = JSON.parse(fs.readFileSync(configPath, 'UTF-8'));
 // We have to export each object in order to access them separately
 exports.mailgun = parsed.mailgun;
 exports.port = parsed.port;
+exports.twilio = parsed.twilio;

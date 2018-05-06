a1d07733ab06e451fc83674f3e6e6bb96ab69b5e
Joseph Ryan <joseph.ryan.iv@gmail.com>
2018-05-06 00:12:41 -0700

    styling changes for list view

--- tasks/config/sails-linker.js
+++ tasks/config/sails-linker.js
@@ -63,7 +63,7 @@ module.exports = function(grunt) {
       options: {
         startTag: '<!--SCRIPTS-->',
         endTag: '<!--SCRIPTS END-->',
-        fileTmpl: '<script src="%s?v=2"></script>',
+        fileTmpl: '<script src="%s?v=3"></script>',
         appRoot: '.tmp/public'
       },
       files: {
